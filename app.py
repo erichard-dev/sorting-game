4d2b803271af2da55f7414e5eea6c7dfe7beab96
erichard-dev <elliot.r@free.fr>
2020-12-01 15:19:58 +0100

    change data model
    add color and order information.

--- app.py
+++ app.py
@@ -11,18 +11,115 @@ def index():
 @app.route('/data')
 def get_data():
     data = { 
-        'riddles': [ 
-            { 
+        'riddles': 
+        [ 
+            {
                 'instruction': 'Ordonner la liste des plantes du systme solaire selon la distance croissante avec le soleil.', 
-                'answer': ["mercure", "venus", "terre", "mars", "jupitère", "saturne", "uranus", "neptune"]
+                'answers' : 
+                [
+
+                    {
+                        'order': 8,
+                        'answer': 'neptune',
+                        'color': 'chartreuse',
+                    },
+                    {
+                        'order': 1,
+                        'answer': 'mercure',
+                        'color': 'red',
+                    },
+                    {
+                        'order': 2,
+                        'answer': 'venus',
+                        'color': 'dodgerblue',
+                    },
+                    {
+                        'order': 3,
+                        'answer': 'terre',
+                        'color': 'dodgerblue',
+                    },
+                    {
+                        'order': 4,
+                        'answer': 'mars',
+                        'color': 'red',
+                    },
+                    {
+                        'order': 5,
+                        'answer': 'jupitere',
+                        'color': 'chartreuse',
+                    },
+                    {
+                        'order': 6,
+                        'answer': 'saturne',
+                        'color': 'yellow',
+                    },
+                    {
+                        'order': 7,
+                        'answer': 'uranus',
+                        'color': 'yellow',
+                    }
+                ]
             },
             { 
-                'instruction': 'Ordonner la liste de nombres dans l\'ordre croissant', 
-                'answer': ["1", "2", "3", "4", "5"]
+                'instruction': 'Ordonner la liste de nombres dans l\'ordre croissant',
+                'answers' : [
+                    {
+                    'order': 1,
+                    'answer': '1',
+                    'color': 'dodgerblue',
+                    },
+                    {
+                        'order': 2,
+                        'answer': '2',
+                        'color': 'dodgerblue',
+                    },
+                    {
+                        'order': 3,
+                        'answer': '3',
+                        'color': 'yellow',
+                    },
+                    {
+                        'order': 4,
+                        'answer': '4',
+                        'color': 'chartreuse',
+                    },
+                    {
+                        'order': 5,
+                        'answer': '5',
+                        'color': 'red',
+                    }
+                ]  
             },
-            { 
-                'instruction': 'Ordonner la liste de nombres dans l\'ordre décroissant', 
-                'answer': ["5", "4", "3", "2", "1"]
+            {
+                'instruction': 'Ordonner la liste de nombres dans l\'ordre décroissant',
+                'answers' : 
+                [
+                    {
+                    'order': 1,
+                    'answer': '5',
+                    'color': 'red',
+                    },
+                    {
+                        'order': 2,
+                        'answer': '4',
+                        'color': 'chartreuse',
+                    },
+                    {
+                        'order': 3,
+                        'answer': '3',
+                        'color': 'yellow',
+                    },
+                    {
+                        'order': 4,
+                        'answer': '2',
+                        'color': 'dodgerblue',
+                    },
+                    {
+                        'order': 5,
+                        'answer': '1',
+                        'color': 'dodgerblue',
+                    }
+                ]   
             }
         ]
     }
